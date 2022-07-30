e912fde6e0a5dea0a8d58965e7e1e036101966fa
Theo Phillips <phillipstheo88@gmail.com>
2022-07-30 16:43:16 -0700

    fixed issue where i put he wrong code in the wrong file.

--- models/index.js
+++ models/index.js
@@ -1,29 +1,4 @@
-// REACTION'S SUBDOCUMENT SCHEMA IN THOUGHT MODEL
+const User = require("./User");
+const Thought = require("./Thought");
 
-const { Schema, Types } = require("mongoose");
-const dateFormat = require("../utils/dateFormat");
-
-const ReactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: "Please enter your reaction",
-            maxLength: 280
-        },
-        username: {
-            type: String,
-            required: "You need a username!",
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal),
-        }
-    }
-);
-
-module.exports = ReactionSchema;
+module.exports = { User, Thought };
